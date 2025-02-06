c8324459be1cc09ed2da07fbc5371e4440aa989d
Xiping Yan <xiping.yan@intel.com>
2025-02-06 09:41:18 +0800

    Update run_vllm_server.sh

--- vllm/run_vllm_server.sh
+++ vllm/run_vllm_server.sh
@@ -1,3 +1,5 @@
+source python-env/bin/activate
+
 export LD_LIBRARY_PATH=/mnt/data_nvme1n1p1/xiping_workpath/deepseek/vllm/python-env/lib/python3.10/site-packages/nvidia/nvjitlink/lib/:$LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES=0
 
@@ -10,7 +12,4 @@ unset HTTPS_PROXY
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
-CUDA_VISIBLE_DEVICE=0 VLLM_HOST_IP=10.67.108.170 vllm serve $model --tensor-parallel-size 1 --max-model-len 24575 --enforce-eager --dtype=half
-
-#--dtype=half
-
+CUDA_VISIBLE_DEVICE=1 VLLM_HOST_IP=10.67.108.170 vllm serve $model --tensor-parallel-size 1 --max-model-len 24575 --enforce-eager --dtype=half
